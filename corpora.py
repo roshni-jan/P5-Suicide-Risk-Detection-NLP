5c314cdfcc1ac1666440f6c0bd57ffeea3d386a3
Roshni Janakiraman <roshnij618@gmail.com>
2023-02-23 14:52:44 -0600

    another streamlit attempt

--- corpora.py
+++ corpora.py
@@ -1,5 +1,6 @@
+import textblob
 import subprocess
 
-cmd = ['python3','-m','textblob.download_corpora']
+cmd = ['python3','-m','textblob.download_corpora lite']
 subprocess.run(cmd)
 print("Word Data Downloaded")
